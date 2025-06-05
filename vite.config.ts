7407ff6b76f2609b4896ac9fe1f938d21ba6707e
Leonardo Rivero Arenas <leonardo.rivero@gse.com.co>
2025-06-05 12:51:42 -0500

    try deploying

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/LMIROF_Front/',
-  plugins: [react()],
+  plugins: [react({
+    babel: {
+      plugins: []
+    }
+  })],
+  build: {
+    outDir: 'dist', // o 'build' si así lo prefieres
+    minify: 'esbuild',
+  }
 })
